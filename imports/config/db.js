57214f067a65d0f72d1a0c14fffa6fc7f7510272
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-11-10 16:41:36 +0500

    using aws mongodb

--- imports/config/db.js
+++ imports/config/db.js
@@ -6,8 +6,9 @@ module.exports = {
         database: 'techeejad'
     },
     mongo: {
-        local: "mongodb://sahs9996:Ahmed1234.!@127.0.0.1:27017/admin",
-        uri: "mongodb+srv://sahs9996:Ahmed1234.!@cluster0-hkp4k.mongodb.net/test?retryWrites=true&w=majority",
+        uri: "mongodb://sahs9996:Ahmed1234.!@127.0.0.1:27017/admin",
+        atlas: "mongodb+srv://sahs9996:Ahmed1234.!@cluster0-hkp4k.mongodb.net/test?retryWrites=true&w=majority",
         database: 'techeejad'
     }
 }
+
