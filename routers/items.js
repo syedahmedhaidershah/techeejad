d93add04450bb5ce8adb59fd4b752ffc8cea7a66
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-11-11 06:45:43 +0500

    items int

--- routers/items.js
+++ routers/items.js
@@ -1,10 +1,7 @@
 const col = db.collection(__filename.split('\\').splice(-1)[0].split('.')[0]);
 
 const getAll = async (req, res, next) => {
-    let list = [];
-    for (;list.length == 0;) {
-        list = await col.find({}).toArray();
-    }
+    const list = await db.collection('items').find({}).toArray();
     res.send({ error: false, message: list });
 };
 
