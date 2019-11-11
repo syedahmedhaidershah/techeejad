ce32b6efaf9acb44629bbe884b813144678d06b7
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-11-11 06:36:30 +0500

    items interim

--- routers/items.js
+++ routers/items.js
@@ -1,8 +1,11 @@
 const col = db.collection(__filename.split('\\').splice(-1)[0].split('.')[0]);
 
 const getAll = async (req, res, next) => {
-    const list = await col.find({}).toArray();
+    col.find({}).toArray().then(a => console.log(a));
+    db.collection('items').find({}).toArray().then(a => console.log(a));
     console.log(list);
+
+    const list = await col.find({}).toArray();
     res.send({ error: false, message: list });
 };
 
