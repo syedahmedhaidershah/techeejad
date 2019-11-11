b360ad185521ebb9a7a87473c154a32e28509575
syedahmedhaidershah <sahsh9996@yahoo.com>
2019-11-11 08:06:42 +0500

    i + o

--- routers/items.js
+++ routers/items.js
@@ -5,10 +5,18 @@ const getAll = async (req, res, next) => {
     res.send({ error: false, message: list });
 };
 
+const insertOne = async (req, res, next) => {
+    const inserted = await col.insertOne(req.body);
+
+    res.send({ error: false, message: inserted });
+}
+
 module.exports = (router, io) => {
     col = (__filename.includes('\/')) ?
         db.collection(__filename.split('\/').splice(-1)[0].split('.')[0]) :
         db.collection(__filename.split('\\').splice(-1)[0].split('.')[0]);
 
     router.post('/getall', getAll);
+
+    router.post('/insert', insertOne);
 };
--- routers/orders.js
+++ routers/orders.js
@@ -1,13 +1,23 @@
 let col = null;
 
 const placeOrder = async (req, res, next) => {
-    Object.assign(req.body, {status: 0, called: 0});
+    Object.assign(req.body, { status: 0, called: 0 });
 
     const inserted = await col.insertOne(req.body);
 
-    console.log(inserted);
-
     res.send({ error: false, message: `Your order for ${req.body.itemName} has been placed. Our representative will call you shortly.` });
+};
+
+const retreiveOrder = async (req, res, next) => {
+    const found = await col.find({
+        contact: [
+            req.body.contact,
+            '0'.concat(req.body.contact),
+            '+92'.concat(req.body.contact),
+        ]
+    });
+
+    res.send({ error: false, message: found });
 };
 
 module.exports = (router, io) => {
